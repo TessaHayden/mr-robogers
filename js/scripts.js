d5ca8f9d313531c946aa0ff2245bb2e0f646e525
Tessa Hayden <tessahayden3@gmail.com>
2023-06-30 11:20:22 -0700

    Add basic HTML elements to run in browser, scripts.js - work in progress - function to beep numbers with 1.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,33 @@
 let userInputNum = 45;
 let countNum = 0;
-const countList = [];
+let countList = [];
+let beepList = [];
+const numToBeep = [1];
 
 for (let i = 0; i <= userInputNum; i++) {
   countList.push(countNum++);
 }
 
-let beepList = [];
+// countList.forEach(function (numW1) {
+//     if (countList.includes(1)) {
+//         beepList.push("beep");
+//     } else {
+//         beepList.push(numW1);
+//     }
+// });
 
 let forEachList = countList.map((el) => {
   return el.toString();
 });
 let searchNum = forEachList.map((el) => {
-  return el.includes("1");
+    if (el.includes("1") === true) {
+        beepList.push("Beep");
+    } else if (el.includes("1") === false) {
+        beepList.push(el);
+     }
 });
+
 // console.log(beepList);
 // console.log(countList);
 // console.log(forEachList);
+console.log(searchNum);
