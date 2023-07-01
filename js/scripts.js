b95011d03a719eb0e58c293ea1ca4aa0fffb6c43
Tessa Hayden <tessahayden3@gmail.com>
2023-07-01 00:02:47 -0700

    Print array to window.

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,41 @@
-let userInputNum = 45;
-let countNum = 0;
-let countList = [];
-let beepList = [];
-const numToBeep = [1];
-
-for (let i = 0; i <= userInputNum; i++) {
-  countList.push(countNum++);
+function replaceNum() {
+    let userInputNum = document.getElementById("userNum");
+    const numToUse = parseInt(userInputNum.value)
+    let countNum = 0;
+    let countList = [];
+    let beepList = [];
+    
+    for (let i = 0; i <= numToUse; i++) {
+      countList.push(countNum++);
+    }
+    
+    let forEachList = countList.map((el) => {
+      return el.toString();
+    });
+    
+    let searchNum1 = forEachList.map((el) => {
+        if (el.includes("3") === true) {
+            beepList.push("Won't you be my neighbor");
+        } else if (el.includes("2") === true){
+            beepList.push("boop");
+        } else if (el.includes("1") === true) {
+            beepList.push("beep");
+        } else {
+            beepList.push(el);
+        }
+    });
+    document.querySelector("p#replace-msg").innerText = beepList;
 }
 
-// countList.forEach(function (numW1) {
-//     if (countList.includes(1)) {
-//         beepList.push("beep");
-//     } else {
-//         beepList.push(numW1);
-//     }
-// });
 
-let forEachList = countList.map((el) => {
-  return el.toString();
-});
 
-let searchNum1 = forEachList.map((el) => {
-    if (el.includes("1") === true) {
-        beepList.push("beep")
-    } else {
-        beepList.push(el)
-    }
-});
-
-let searchNum2 = forEachList.forEach((el) => {
-    if (el.includes("2") === true) {
-        beepList.push("boop")
-    }
+window.addEventListener("load", function () {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        replaceNum();
+    });
 })
 
-let searchNum3 = forEachList.forEach((el) => {
-    if (el.includes("3") === true) {
-        beepList.push("Won't you be my neighbor?")
-    }
-})
-console.log(beepList);
-// console.log(countList);
-// console.log(forEachList);
-// console.log(searchNum);
+
+
