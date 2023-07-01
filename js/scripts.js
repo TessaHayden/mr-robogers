f5474fc2b2ba483da201f7092894ccbf8a1bd955
Tessa Hayden <tessahayden3@gmail.com>
2023-07-01 00:55:23 -0700

    Add logic to create a hierarchy of substitutions and a button to reset input value and clear the array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 function replaceNum() {
     let userInputNum = document.getElementById("userNum");
-    const numToUse = parseInt(userInputNum.value)
+    const numToUse = parseInt(userInputNum.value);
     let countNum = 0;
     let countList = [];
     let beepList = [];
@@ -14,7 +14,7 @@ function replaceNum() {
     });
     
     let searchNum1 = forEachList.map((el) => {
-        if (el.includes("3") === true) {
+        if (el.includes("3" || "2") === true) {
             beepList.push("Won't you be my neighbor");
         } else if (el.includes("2") === true){
             beepList.push("boop");
@@ -31,11 +31,18 @@ function replaceNum() {
 
 window.addEventListener("load", function () {
     const form = document.querySelector("form");
+    const resetBtn = document.getElementById("reset-btn"); 
     form.addEventListener("submit", function (event) {
         event.preventDefault();
         replaceNum();
     });
+    resetBtn.addEventListener("click", function () {
+        document.querySelector("input#userNum").value = "";
+        document.querySelector("p#replace-msg").innerText = "";
+    });
 })
+
+
 
 
 
